e07965efb74090453e9cf8abdea27c47e96c901d
dingoatemybabiesss <ghoniemohamed@gmail.com>
2025-11-24 00:16:13 +0200

    renamed:    src/config/.env -> .env 	modified:   package.json 	deleted:    server.js 	deleted:    src/config/config.js 	renamed:    src/controllers/authController.js -> src/controllers/auth.controller.js 	renamed:    src/middleware/authMiddleware.js -> src/middleware/auth.middleware.js 	deleted:    src/models/User.js 	renamed:    src/routes/authRoutes.js -> src/routes/auth.routes.js 	renamed:    src/app.js -> src/server.js 	renamed:    src/services/authService.js -> src/services/auth.service.js

--- src/server.js
+++ src/server.js
@@ -1,30 +1,31 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require('cors');
-const authRoutes = require("./routes/authRoutes");
-const initializeDatabase = require('./config/db');
+const initializeDatabase = require('./database/db');
+
+// Import Routes
+const authRoutes = require("./routes/auth.routes");
 const userRoutes = require('./routes/user.routes');
 const restaurantRoutes = require('./routes/restaurant.routes');
 const orderRoutes = require('./routes/order.routes');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
 app.use(cors());
 app.use(express.json());
 
-// Mount auth routes
+// Mount Routes
 app.use("/api/auth", authRoutes);
-
-const PORT = process.env.PORT || 3000;
+app.use('/users', userRoutes);
+app.use('/restaurants', restaurantRoutes);
+app.use('/orders', orderRoutes);
 
 async function startServer() {
     try {
         const db = await initializeDatabase();
         app.locals.db = db;
         console.log("Database connected and attached.");
-
-        app.use('/users', userRoutes);
-        app.use('/restaurants', restaurantRoutes);
-        app.use('/orders', orderRoutes);
 
         app.listen(PORT, () => {
           console.log(`Server running on port ${PORT}`);
